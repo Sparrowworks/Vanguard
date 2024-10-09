50171e7570957d43863d958f7f4f44f2c1301615
VargaDot <vancouver_cliparf@simplelogin.com>
2024-10-09 16:14:08 +0100

    Update weapon.gd
    Added two export groups

--- base/weapon.gd
+++ base/weapon.gd
@@ -9,8 +9,12 @@ extends Node
 @export var reload_time_empty:int
 @export var recoil:int
 @export var payload:PackedScene # Place a bullet, melee weapon ext...
-@export var has_silencer:bool
 @export_enum("Automatic", "Semi", "Burst") var fire_mode:int
+
+@export_group("Noise Stats")
+@export var noise_field:PackedScene
+@export var has_silencer:bool
+@export var noise_radius:int
 
 @export_group("Payload Stats")
 @export_enum("Projectile", "Melee") var payload_type:int
@@ -18,12 +22,14 @@ extends Node
 @export var accuracy:float # 1.0 very accurate / 0.0 complete trash
 @export var max_spread:int # the maximum angle in which the bullet the launched when at 0 accuracy
 @export var p_range:int # to evade warning "The variable "range" has the same name as a built-in function."
-@export var radius:int
-@export var propagation_speed:float # speed at which a field expands and reaches its max radius
-@export var field_fade_time:int # time it takes for the field to be removed once it reached its full radius
 @export var speed:int
 @export var knockback:int
 @export var crit_chance:int
+
+@export_group("Payload Field Stats")
+@export var radius:int
+@export var propagation_speed:float # speed at which a field expands and reaches its max radius
+@export var field_fade_time:int # time it takes for the field to be removed once it reached its full radius
 
 var TIMER:Timer
 func _ready() -> void:
