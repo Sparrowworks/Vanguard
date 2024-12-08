ec93d1dc09d265dad37f2443d67fcc620d12aec8
VargaDot <vancouver_cliparf@simplelogin.com>
2024-12-08 17:56:40 +0100

    kits

---
+++ blueprints/kit/gun_kit.gd
@@ -0,0 +1,19 @@
+extends Resource
+
+## Base class for gun addons/attachments modifiers.
+##
+## To create a new kit (or attachment), create a custom resource and assign it this script.
+## Simply write a percentage and modify_weapon_stats will take care of the math,
+## your only limitation is how much the float type can handle.
+
+## The percentage by which to increase or decrease the size of the magazine.
+@export var mag_size_modifier:float
+## The percentage by which to increase or decrease the maximum ammunition.
+@export var max_ammo_modifier:float
+## The percentage by which to increase or decrease the reload speed.
+@export var reload_time_modifier:float
+## The percentage by which to increase or decrease the reload speed when the magazine is empty.
+## (reload time + reload time empty = actual reload time).
+@export var reload_time_empty_modifier:float
+## The percentage by which to increase or decrease the rate of fire.
+@export var fire_rate_modifier:float
