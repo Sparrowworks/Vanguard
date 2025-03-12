3fffa07300a33149c8aab9775bee644e72c712e6
VargaDot <vancouver_cliparf@simplelogin.com>
2025-03-12 17:08:58 +0100

    attack, recover and reset to ready

--- blueprints/melee/melee.gd
+++ blueprints/melee/melee.gd
@@ -6,6 +6,9 @@ class_name Melee extends Node2D
 ## It manages weapon stats such as "INSERT NO DOCUMENTATION HERE".
 ## This class is designed to handle various weapon functionalities, including attacking and recovering.
 
+var attack_rate:int
+var recovery_time:int
+
 ## Emitted when current_state is changed
 signal state_updated()
 ## Emitted when current_state is ready
@@ -38,6 +41,37 @@ enum WEAPON_STATE {
 	## The weapon is recovering.
 	RECOVERING = 3,
 }
+
+## Used to manage delays between firing and reloading actions.
+## Sets current weapon state to READY when it timesout.
+var ranged_timer:Timer
+func _init() -> void:
+	ranged_timer = Timer.new()
+	ranged_timer.one_shot = true
+	add_child(ranged_timer)
+	ranged_timer.timeout.connect(_on_weapon_timer_timeout)
+
+## The shoot() method is responsible for handling the firing mechanism of the weapon.
+## If there is no ammunition, it will reload.
+## If the weapon is currently firing or reloading, it exits without firing.
+func attack() -> void:
+	if (current_state != WEAPON_STATE.READY):
+		return
+
+	current_state = WEAPON_STATE.ATTACKING
+	ranged_timer.wait_time = attack_rate
+	ranged_timer.start()
+
+func recover() -> void:
+	if (current_state != WEAPON_STATE.READY):
+		return
+
+	current_state = WEAPON_STATE.RECOVERING
+	ranged_timer.wait_time = recovery_time
+	ranged_timer.start()
+
+func _on_weapon_timer_timeout() -> void:
+	current_state = WEAPON_STATE.READY
 
 func _enum_to_str(state: int) -> String:
 	match state:
