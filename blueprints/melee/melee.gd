38c7c01afd266433204d835c75a008636700f8cd
VargaDot <vancouver_cliparf@simplelogin.com>
2025-03-12 17:47:53 +0100

    Melee kits

--- blueprints/melee/melee.gd
+++ blueprints/melee/melee.gd
@@ -6,6 +6,20 @@ class_name Melee extends Node2D
 ## It manages weapon stats such as "INSERT NO DOCUMENTATION HERE".
 ## This class is designed to handle various weapon functionalities, including attacking and recovering.
 
+@export_group("Melee Stats")
+@export var base_stats:MeleeStatKit:
+	set(val):
+		if val:
+			attack_rate = val.attack_rate
+			recovery_time = val.recovery_time
+			base_stats = val
+
+@export_group("Melee Emissions")
+## Slot for field
+@export var field:PackedScene
+## Slot for projectile
+@export var projectile:PackedScene
+
 var attack_rate:int
 var recovery_time:int
 
@@ -17,6 +31,12 @@ signal weapon_ready()
 signal weapon_attacking()
 ## Emitted when current_state is recovering
 signal weapon_recovering()
+## Emitted when a MeleeStatKit is equipped
+signal stat_kit_equipped(kit:MeleeStatKit)
+## Emitted when a MeleeStatKit is unequipped
+signal stat_kit_unequipped(kit:MeleeStatKit)
+## Emitted when a RangedEmissionKit is equipped
+signal emission_kit_equipped(kit:RangedEmissionKit)
 
 ## Represents the current state of the weapon.
 ## It can be one of the states defined in the WEAPON_STATE enum.
@@ -70,6 +90,49 @@ func recover() -> void:
 	current_state = WEAPON_STATE.RECOVERING
 	ranged_timer.wait_time = recovery_time
 	ranged_timer.start()
+
+## A list containing the names of currently equipped modifications
+var equipped_kits:Array[String]
+## Modifies weapon stats by providing a kit.
+func equip_stat_kit(kit:MeleeStatKit) -> void:
+	if has_stat_kit(kit):
+		print("kit already installed")
+		return
+
+	equipped_kits.append(kit.kit_name)
+	attack_rate += kit.attack_rate if kit.attack_rate_modifier != 0 else attack_rate
+	recovery_time += kit.recovery_rate_modifier if kit.recovery_rate_modifier !=0 else recovery_time
+
+	stat_kit_equipped.emit(kit)
+
+## Reverses the changes made by equip_kit
+func unequip_stat_kit(kit:MeleeStatKit) -> void:
+	equipped_kits.erase(kit.kit_name)
+	attack_rate -= kit.attack_rate_modifier
+	recovery_time -= kit.recovery_rate_modifier
+
+	stat_kit_unequipped.emit(kit)
+
+func has_stat_kit(kit: MeleeStatKit) -> bool:
+	for kit_equipped in equipped_kits:
+		if (kit_equipped == kit.kit_name):
+			return true
+
+	return false
+
+## Changes the weapon's emissions, null variables will be ignored.
+func equip_emission_kit(kit:RangedEmissionKit) -> void:
+	if (kit.new_projectile != null):
+		projectile = kit.new_projectile
+	else:
+		print("Null projectile detected, New projectile rejected...")
+
+	if (kit.new_field != null):
+		field = kit.new_field
+	else:
+		print("Null field detected, New field rejected...")
+
+	emission_kit_equipped.emit(kit)
 
 func _on_weapon_timer_timeout() -> void:
 	current_state = WEAPON_STATE.READY
