30da2112acbb36bbad5fae3bb8a32e91e4a00960
VargaDot <vancouver_cliparf@simplelogin.com>
2025-03-29 17:53:46 +0100

    document hitbox

--- blueprints/hitbox/hitbox.gd
+++ blueprints/hitbox/hitbox.gd
@@ -1,6 +1,12 @@
 class_name Hitbox extends Area2D
 
+## Class for detection by [Hurtbox]
+##
+## Set a predefined [member Hitbox.hitbox_layer] here and assign the same value to [member Hurtbox.hurtbox_mask]
+
+## [member CollisionObject2D.collision_layer] used to be detected by [member CollisionObject2D.collision_mask]
 var hitbox_layer:int = 2
+## [member CollisionObject2D.collision_mask] used to detect [member CollisionObject2D.collision_layer]
 var hitbox_mask:int = 0
 
 func _init() -> void:
