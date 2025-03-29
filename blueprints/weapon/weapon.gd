0a7afb2b4fbc55af271464834b2decb76fa377d0
VargaDot <vancouver_cliparf@simplelogin.com>
2025-03-29 22:43:19 +0100

    Added BBcode tags

--- blueprints/weapon/weapon.gd
+++ blueprints/weapon/weapon.gd
@@ -1,38 +1,41 @@
 class_name Weapon extends Node
 
-## Base class for all weapons
+## Base class for all weapons.
 ##
-## The weapon class contains a state machine, signal management.
+## The weapon class contains a [url=https://www.youtube.com/watch?v=sNFMdLEVxFo]state machine[/url],
+## [Signal] management and basic [Weapon] functionality.
 
 #region State Machine
-## Emitted when current_state is changed.
+## Emitted when [member current_state] is changed.
 signal state_updated(state: String)
-## Emitted when current_state is ready.
+## Emitted when [member current_state] is ready.
 signal weapon_ready()
-## Emitted when current_state is charging.
+## Emitted when [member current_state] is charging.
 signal weapon_charging()
-## Emitted when current_state is attacking.
+## Emitted when [member current_state] is attacking.
 signal weapon_attacking()
-## Emitted when current_state is recovering.
+## Emitted when [member current_state] is recovering.
 signal weapon_recovering()
 
 ## A list of possible weapon states.
 enum WEAPON_STATE {
-	## The weapon system is being initialized.
+	## The [Weapon] system is being initialized.
 	INITIALIZE = 0,
-	## The weapon is ready (can be toggled to anything below).
+	## The [Weapon] is ready (can be toggled to anything below).
 	READY = 1,
-	## The weapon is charging (ex: charging a rifle).
+	## The [Weapon] is charging (ex: charging a rifle).
 	CHARGING = 2,
-	## The weapon is attacking (ex: shooting).
+	## The [Weapon] is attacking (ex: shooting).
 	ATTACKING = 3,
-	## The weapon is recovering (ex: reloading).
+	## The [Weapon] is recovering (ex: reloading).
 	RECOVERING = 4,
 }
 
-## Represents the current state of the weapon,
-## It can be one of the states defined in the WEAPON_STATE enum.
-## Emits two signals, state_updated and the corresponding weapon state's signal (if READY then weapon_ready).
+## Represents the current state of the [Weapon],
+## It can be one of the states defined in the [enum WEAPON_STATE].
+## [br]Emits two signals, [signal state_updated] and the corresponding [enum WEAPON_STATE]'s signal,
+## (if [enum READY] then [signal weapon_ready]).
+## [br]Starts off at [enum INITIALIZE].
 var current_state: int = WEAPON_STATE.INITIALIZE:
 	set(val):
 		current_state = val
@@ -47,29 +50,31 @@ var current_state: int = WEAPON_STATE.INITIALIZE:
 #endregion
 
 #region Initialization logic
-## Used to keep track of how long the weapon stays in one state before resetting to READY
+## Used to keep track of how long the [Weapon] stays in one state before resetting to [enum READY]
 var weapon_timer: Timer
 
+# Exists so that the user doesn't need to create a timer everytime they make a new weapon!
 func _init() -> void:
 	weapon_timer = Timer.new()
 	weapon_timer.one_shot = true
 	add_child(weapon_timer)
 	weapon_timer.timeout.connect(on_weapon_timer_timeout)
 
+# Signals it's ready, useful for giving UI stats and all.
 func _ready() -> void:
 	current_state = WEAPON_STATE.READY
 #endregion
 
 #region Weapon Functionality
-## Time (in milliseconds) it takes to charge a weapon fully
+## Time (in milliseconds) it takes to [method charge] a [Weapon] fully.
 var charge_rate: float
-## Time (in milliseconds) it takes to finish an attack before moving to another action/attack again
+## Time (in milliseconds) it takes to finish an [method attack] before making another action.
 var attack_rate: float
-## Time (in milliseconds) it takes to finish the recovery phase
+## Time (in milliseconds) it takes to finish a [method recover] phase.
 var recovery_rate: float
 
-## Handles the weapon's charging mechanism,
-## It will exits without charging if it's not READY or CHARGING.
+## Handles the [Weapon]'s [enum CHARGING] mechanism,
+## It will exits without charging if it's not [enum READY] or [enum CHARGING].
 func charge() -> void:
 	if (current_state != WEAPON_STATE.READY || WEAPON_STATE.CHARGING):
 		return
@@ -81,8 +86,8 @@ func charge() -> void:
 
 	pass
 
-## Handles the weapon's attacking mechanism,
-## It will exits without attacking if it's not READY.
+## Handles the [Weapon]'s [enum ATTACKING] mechanism,
+## It will exits without [enum ATTACKING] if it's not [enum READY].
 func attack() -> void:
 	if (current_state != WEAPON_STATE.READY):
 		return
@@ -91,8 +96,8 @@ func attack() -> void:
 	weapon_timer.wait_time = attack_rate
 	weapon_timer.start()
 
-## Handles the weapon's recovery mechanism,
-## It will exits without recovering if it's not READY.
+## Handles the [Weapon]'s [enum RECOVERING] mechanism,
+## It will exits without [enum RECOVERING] if it's not [enum READY].
 func recover() -> void:
 	if (current_state != WEAPON_STATE.READY):
 		return
@@ -101,13 +106,15 @@ func recover() -> void:
 	weapon_timer.wait_time = recovery_rate
 	weapon_timer.start()
 
-## Governs what happens when the weapon finished an action.
+## Governs what happens when the [Weapon] finished an action.
+## Resets to [enum READY]
 func on_weapon_timer_timeout() -> void:
 	current_state = WEAPON_STATE.READY
 #endregion
 
 #region Misc
-# Translates enums to strings to output a string for the signals
+# Translates enums to strings, outputs a string.
+# Used at current_state setter to emit the state_updated signal.
 func _enum_to_str(state: int) -> String:
 	match state:
 		0:
