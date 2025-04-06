2391c376f66fc6d4b153100f66a6db87100eb833
Varga <110741996+VargaDot@users.noreply.github.com>
2025-04-06 14:37:52 +0100

    Added regions to hurtbox

--- blueprints/hurtbox/hurtbox.gd
+++ blueprints/hurtbox/hurtbox.gd
@@ -4,6 +4,7 @@ class_name Hurtbox extends Area2D
 ##
 ## Assign [member Hitbox.hitbox_layer] to [member Hurtbox.hurtbox_mask] to run detection logic.
 
+#region Initialization Logic
 ## [member CollisionObject2D.collision_layer] used to be detected by [member CollisionObject2D.collision_mask].
 var hurtbox_layer: int = 0
 ## [member CollisionObject2D.collision_mask] used to detect [member CollisionObject2D.collision_layer].
@@ -17,7 +18,9 @@ func _init() -> void:
 ## Connects [signal area_entered] to [method on_hurtbox_entered].
 func _ready() -> void:
 	area_entered.connect(on_hurtbox_entered)
+#endregion
 
+#region Detection Logic
 ## Activates when [signal area_entered] is emitted by detecting a [member Hitbox.collision_layer],
 ## Checks if the [Hitbox] contains [method take_damage] and executes it.
 ## [br][br][code]return[/code] if [Hitbox] is [code]null[/code]  
@@ -31,3 +34,4 @@ func on_hurtbox_entered(hitbox: Hitbox) -> void:
 # Seems random yes, but the only "hitbox" here with a timer is a projectile.
 		if hitbox.has_node("./Timer"):
 			hitbox.queue_free()
+#endregion
