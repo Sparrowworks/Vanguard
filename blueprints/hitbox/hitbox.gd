c4df1becb9fb2692fcfbb79ab48e774d9cad7a1e
Varga <110741996+VargaDot@users.noreply.github.com>
2025-04-06 14:57:49 +0100

    Hitbox [CollisionObject2D.xxx]

--- blueprints/hitbox/hitbox.gd
+++ blueprints/hitbox/hitbox.gd
@@ -2,7 +2,7 @@ class_name Hitbox extends Area2D
 
 ## Class for detection by [Hurtbox].
 ##
-## Set a predefined [member Hitbox.hitbox_layer] here and assign the same value to [member Hurtbox.hurtbox_mask].
+## Set a predefined [member hitbox_layer] here and assign the same value to [member Hurtbox.hurtbox_mask].
 
 #region Initialisation Logic
 ## [member CollisionObject2D.collision_layer] used to be detected by [member CollisionObject2D.collision_mask].
@@ -10,7 +10,7 @@ class_name Hitbox extends Area2D
 ## [member CollisionObject2D.collision_mask] used to detect [member CollisionObject2D.collision_layer].
 @export var hitbox_mask: int = 0
 
-## Assigns the exported values to [member collision_layer] and [member collision_mask].
+## Assigns the exported values to [member CollisionObject2D.collision_layer] and [member CollisionObject2D.collision_mask].
 func _init() -> void:
 	collision_layer = hitbox_layer
 	collision_mask = hitbox_mask
